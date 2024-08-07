1b26e7928df5b319576dcc569e93b8267e84152a
Farideh <heidarian89@gmail.com>
2024-08-07 07:14:09 +0330

    create ProductCard.tsx and update home

--- app/(frontend)/(home)/page.tsx
+++ app/(frontend)/(home)/page.tsx
@@ -1,3 +1,15 @@
+import ProductCard from "@/components/product/ProductCard";
+import data from "@/lib/data/data";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h2 className="text-2xl ">Latest Products</h2>
+      <div>
+        {data.products.map((p) => (
+          <ProductCard key={p.slug} />
+        ))}
+      </div>
+    </>
+  );
 }
