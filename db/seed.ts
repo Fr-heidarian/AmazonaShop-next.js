34a768bc00f016e43393f9f261c223df195d0de0
Farideh <heidarian89@gmail.com>
2024-08-17 12:40:51 +0330

    Insert initial product data into the database - Implement seed.ts

---
+++ db/seed.ts
@@ -0,0 +1,24 @@
+import "@/db/envConfig";
+
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
+import data from "@/lib/data/data";
+import { Client } from "pg";
+
+const main = async () => {
+  try {
+    const client = new Client({ connectionString: process.env.POSTGRES_URL });
+    await client.connect();
+    const db = drizzle(client);
+    const res = await db
+      .insert(schema.products)
+      .values(data.products)
+      .returning();
+
+    await client.end();
+  } catch (e) {
+    throw new Error("Failed to seed database");
+  }
+};
+
+main();
