05f2886e7f06520a49a20a70448e8be88e0d51ed
Farideh <heidarian89@gmail.com>
2024-08-25 00:05:12 +0330

    add increase and decrease functionality for cart items

--- lib/hooks/useCartStore.ts
+++ lib/hooks/useCartStore.ts
@@ -3,6 +3,7 @@ import { persist } from "zustand/middleware";
 import { OrderItem } from "../models/OrderModel";
 
 type Cart = { items: OrderItem[]; totalPrice: number };
+
 const initialState: Cart = { items: [], totalPrice: 0 };
 
 export const cartStore = create<Cart>()(
@@ -11,4 +12,36 @@ export const cartStore = create<Cart>()(
 
 export default function useCartService() {
   const { items, totalPrice } = cartStore();
+
+  return {
+    items,
+    totalPrice,
+    increase: (item: OrderItem) => {
+      const exist = items.find((x) => x.slug === item.slug);
+      const updatedCartItems = exist
+        ? items.map((x) =>
+            x.slug === item.slug ? { ...exist, qty: exist.qty + 1 } : x
+          )
+        : [...items, { ...item, qty: 1 }];
+
+      const totalPrice = items.reduce((acc, x) => acc + x.price * x.qty, 0);
+      cartStore.setState({ items: updatedCartItems, totalPrice });
+    },
+
+    decrease: (item: OrderItem) => {
+      const exist = items.find((x) => x.slug === item.slug);
+
+      if (!exist) return;
+
+      const updatedCartItems =
+        exist.qty === 1
+          ? items.filter((x) => x.slug !== item.slug)
+          : items.map((x) =>
+              x.slug === item.slug ? { ...exist, qty: exist.qty - 1 } : x
+            );
+
+      const totalPrice = items.reduce((acc, x) => acc + x.price * x.qty, 0);
+      cartStore.setState({ items: updatedCartItems, totalPrice });
+    },
+  };
 }
