f7e6467cb18943ce1f66145b4a543eb92a193e47
Farideh <heidarian89@gmail.com>
2024-08-26 11:35:19 +0330

    Add quantity controls to AddToCart component

--- components/product/AddToCart.tsx
+++ components/product/AddToCart.tsx
@@ -5,8 +5,15 @@ import { OrderItem } from "@/lib/models/OrderModel";
 type props = { item: OrderItem };
 
 export default function AddToCart({ item }: props) {
-  const { increase } = useCartService();
-  return (
+  const { increase, decrease, items } = useCartService();
+  const existItem = items.find((x) => x.slug === item.slug);
+  return existItem ? (
+    <>
+      <button onClick={() => decrease(item)}>-</button>
+      <span>{existItem.qty}</span>
+      <button onClick={() => increase(item)}>+</button>
+    </>
+  ) : (
     <button
       className="btn bg-rose-500 text-white w-full font-semibold"
       onClick={() => increase(item)}
