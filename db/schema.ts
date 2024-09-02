2c57fd42f6ad2ac9adc681c2ca562eb951fc1ea1
Farideh <heidarian89@gmail.com>
2024-09-02 08:02:11 +0330

    create orders table

--- db/schema.ts
+++ db/schema.ts
@@ -6,6 +6,7 @@ import {
   boolean,
   time,
   numeric,
+  timestamp,
 } from "drizzle-orm/pg-core";
 
 export const products = pgTable("products", {
@@ -14,7 +15,7 @@ export const products = pgTable("products", {
   slug: text("slug").notNull(),
   category: text("category").notNull(),
   image: text("image").notNull(),
-  price: numeric("price", { precision: 2}).notNull().default("0"),
+  price: numeric("price", { precision: 2 }).notNull().default("0"),
   rating: numeric("rating", { precision: 3, scale: 2 }).notNull().default("0"),
   brand: text("brand").notNull(),
   numReviews: integer("numReviews").notNull(),
@@ -34,4 +35,19 @@ export const users = pgTable("users", {
   image: text("image"),
   address: text("address"),
   createdAt: time("createdAt").defaultNow().notNull(),
+});
+
+export const orders = pgTable("order", {
+  id: uuid("id").defaultRandom().primaryKey().notNull(),
+  userId: uuid("id")
+    .defaultRandom()
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  shippingAddress: text("shippingAddress"),
+  paymentMethod: text("paymentMethod"),
+  totalPrice: numeric("totalPrice"),
+  isPaid: boolean("isPaid"),
+  paidAt: timestamp("paidAt"),
+  isDelivered: boolean("isDelivered"),
+  createdAt: timestamp("createdAt").notNull(),
 });
